3eeb6fd1cd5221205a55cc4383a3bd142696ab46
30526 <taiburrahamanapu@gmail.com>
2025-09-14 18:55:20 +0600

    feat: added a card and js function to dynamically add level words

--- script/index.js
+++ script/index.js
@@ -4,13 +4,34 @@ const loadLessons = () => {
         .then(json => displayLessons(json.data))
 }
 
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayLevelWords(data.data))
+}
+
+const displayLevelWords = (words) => {
+    const wordContainer = document.getElementById('word-container')
+    wordContainer.innerHTML = ''
+    words.forEach(words => {
+        const card = document.createElement('div')
+        card.innerHTML = `
+        <p>Cat</p>
+        `
+        wordContainer.append(card)
+    })
+}
+
+
+
 const displayLessons = (lessons) => {
     const levelContainer = document.getElementById('level-container')
     levelContainer.innerHTML = ''
     lessons.forEach(lessons => {
         const btnDiv = document.createElement('div')
         btnDiv.innerHTML = `
-        <button
+        <button onclick="loadLevelWord(${lessons.level_no})"
          class="btn btn-outline btn-primary">
          <span><i class="fa-solid fa-book-open"></i></span>
          Lessons - ${lessons.level_no}
